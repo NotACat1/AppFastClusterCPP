3dfc76357d114a0148dace1aecdebeb30ea7e166
NotACat1 <notacat.mail@gmail.com>
2026-03-24 18:09:33 +0300

    feat: add AoS layout benchmark with iterative sum

--- AppFastClusterCPP/main.cpp
+++ AppFastClusterCPP/main.cpp
@@ -1,36 +1,9 @@
-﻿#include <vector>
-#include <thread>
-#include <chrono>
+﻿#include <benchmark/benchmark.h>
 
-void ProcessData(int id) {
-    DEBUG_TIMER("ProcessData Thread");
-    DEBUG_LOG("Thread starting: " + std::to_string(id));
-
-    // Simulate some work
-    std::this_thread::sleep_for(std::chrono::milliseconds(id * 100));
-
-    if (id == 3) {
-        DEBUG_ERR("Simulated failure in thread " + std::to_string(id));
-    }
-}
-
-int main() {
-    DEBUG_TIMER("Main function total execution");
-    DEBUG_LOG("Application Started");
-
-    // 1. Thread Safety Test
-    std::vector<std::thread> threads;
-    for (int i = 1; i <= 3; ++i) {
-        threads.emplace_back(ProcessData, i);
-    }
-
-    for (auto& t : threads) {
-        t.join();
-    }
-
-    // 2. Memory Leak Detection Test (Visible in MSVC Output)
-    int* intentionalLeak = new int[500]; // We won't delete this!
-
-    DEBUG_LOG("Application shutting down. Check for memory leaks!");
-    return 0;
-}
+/**
+ * @brief Entry point for the Google Benchmark suite.
+ * * This macro generates a main() function that automatically discovers and
+ * executes all registered benchmarks (BM_AoS_Sum, BM_SoA_Sum, etc.),
+ * reporting execution time and CPU cycles.
+ */
+BENCHMARK_MAIN();
